9cf1f54027c3ed79cc6680129e3bc896c0947610
Surasith <surasith@Surasiths-MacBook-Pro.local>
2020-02-21 11:02:25 +0700

    Add listReceipt method in Smart contract and test to interact with SmartContract

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const ethers = require('ethers');
-const contractJSON = require('./build/contracts/KeepSlip.json')
+const ethers = require("ethers");
+const contractJSON = require("./build/contracts/KeepSlip.json");
 
 const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
 
-let privateKey = '1a1d3bd89ce1cb87951963cc310cbffdd939c00a13c29cd78e23e36f85d4fe04';
+let privateKey =
+  "b3caecd0278206321e03889f0c97db39b0806ff1425a0b0567c58cce5c0f3167";
 let wallet = new ethers.Wallet(privateKey, provider);
 
 // init contract
-const contractAddress ='0x1729571758B196F3b322328f0c04fB12bedb383D'
-const contractABI = contractJSON.abi
+const contractAddress = "0xFb8eD82d21785727e1EC9F9dbb7652294E31e976";
+const contractABI = contractJSON.abi;
 let contract = new ethers.Contract(contractAddress, contractABI, provider);
 // console.log(contract);
 
@@ -16,35 +17,97 @@ let contract = new ethers.Contract(contractAddress, contractABI, provider);
 let contractWithSigner = contract.connect(wallet);
 
 // Calling a Non-Constant Method (contract with sign wallt variable)
-addReceipt = (recId, items) =>{
-    contractWithSigner.add(recId, items).then(result => {console.log(result)}).catch(err =>{console.log(err);});
-    // contractWithSigner.add(["a123", "A021684"], [["a123", "Espresso", 4500, 1, false, 0 ],["a123", "IPhoneXI", 4500000, 1, true, 365 ]]).then(result => {console.log(result)}).catch(err =>{console.log(err);});
-}
-let rec = ["a129", "A021684"]
-let items = [["a129", "Espresso", 4500, 1, false, 0 ],["a129", "IPhoneXI", 4500000, 1, true, 365 ]]
-// addReceipt(rec, items)
+addReceipt = (recId, items) => {
+  contractWithSigner
+    .add(recId, items)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  // contractWithSigner.add(["a123", "A021684", "asdqsd3"],[["a123", "Espresso", 4500, 1, false, 0 ],["a123", "IPhoneXI", 4500000, 1, true, 365 ]]).then(result => {console.log(result)}).catch(err =>{console.log(err);});
+};
+// test input
+// ["a123", "A021684", "asdqsd3"],[["a123", "Espresso", 4500, 1, false, 0],["a123", "IPhoneXI", 4500000, 1, true, 365]]
+// ["AP245", "A021684", "ad12345"],[["AP245", "MacBook Pro", 4599000, 1, true, 365],["AP245", "IPhoneXI", 4500000, 1, true, 365]]
+
+let rec = ["a123", "A021684", "asdqsd3"];
+let items = [
+  ["a123", "Espresso", 4500, 1, false, 0],
+  ["a123", "IPhoneXI", 4500000, 1, true, 365]
+];
+// addReceipt(rec, items);
 // addReceipt()
 
 // Calling a read-only Constant Method (Use contract variable)
-getReciptInfomation =(receiptId)=>{
-    contract.getAllItemFromReceipt(receiptId).then(result =>{console.log(result)}).catch(err =>{console.log(err)})
-}
-getReciptInfomation("a129")
+getReciptInfomation = receiptId => {
+  contract
+    .getAllItemFromReceipt(receiptId)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+// getReciptInfomation("a123");
 
-getItem =(receiptId, itemNumber)=>{
-    contract.getItem(receiptId, itemNumber).then(result =>{console.log(result)}).catch(err =>{console.log(err)})
-}
-// getItem("a129", 0)
+getItem = (receiptId, itemNumber) => {
+  contract
+    .getItem(receiptId, itemNumber)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  //  หรือ
+  // contract
+  // .items(receiptId, itemNumber)
+  // .then(result => {
+  //   console.log(result);
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // });
+};
+// getItem("a123", 0);
 
-getItem= async()=>{
-    try{
-        let item =  await contract.getItem("a129", 0)
-        let item0 = {recId: item[0], productName:item[1],  price:parseInt(item[2]),amount:parseInt(item[3]), warranty:item[4], warrantyTime:parseInt(item[5])}
-        // console.log(item0);
-        // console.log(item);
-    }catch(err){
-        console.log(err.data.name);
-    }
-}
+getItem = async () => {
+  try {
+    let item = await contract.getItem("a129", 0);
+    let item0 = {
+      recId: item[0],
+      productName: item[1],
+      price: parseInt(item[2]),
+      amount: parseInt(item[3]),
+      warranty: item[4],
+      warrantyTime: parseInt(item[5])
+    };
+    // console.log(item0);
+    // console.log(item);
+  } catch (err) {
+    console.log(err.data.name);
+  }
+};
 // getItem()
 
+getReceiptList = async () => {
+  try {
+    let list = await contract.listReceipt();
+    // let list = await contract.ReceiptIds(1);
+    console.log(list);
+
+    // for (let i = 0; i < list.length; i++) {
+    //   let rec = await contract.ReceiptIds(i);
+    //   console.log(rec);
+    // }
+
+    // เช็คว่าอยู่ใน array มั้ย
+    // console.log(list.includes("a123a"));
+  } catch (err) {
+    console.log(err);
+  }
+};
+getReceiptList();
--- contracts/KeepSlip.sol
+++ contracts/KeepSlip.sol
@@ -1,57 +1,94 @@
 pragma solidity >=0.4.22 <0.6.0;
 pragma experimental ABIEncoderV2;
-contract KeepSlip{
-    struct Receipt{
+contract KeepSlip {
+    struct Receipt {
         string receiptId;
         string customerId;
+        string storeId;
     }
 
-    mapping (string => Receipt) public receipts;
+    mapping(string => Receipt) public receipts;
     string[] public ReceiptIds;
-    
-    struct Item{
+
+    struct Item {
         string receiptId;
         string productName;
-        uint price; // thb * 100
-        uint amount;
+        uint256 price; // thb * 100
+        uint256 amount;
         bool warranty;
-        uint warrantyTime; // day
+        uint256 warrantyTime; // day
     }
-    
+
     Item[] public ItemArray;
-    mapping (string => Item[]) public items;
-    
+    mapping(string => Item[]) public items;
+
     function add(Receipt memory _receipt, Item[] memory _items) public {
         Receipt memory newReceipt = _receipt;
-        receipts[newReceipt.receiptId] = newReceipt; 
-    
-        for(uint i=0; i<_items.length; i++){
+        receipts[newReceipt.receiptId] = newReceipt;
+
+        for (uint256 i = 0; i < _items.length; i++) {
             Item memory newItem = _items[i];
             items[newReceipt.receiptId].push(newItem);
-        } 
-        
+        }
+
         ReceiptIds.push(newReceipt.receiptId);
     }
-    
-    function getReceiptInformation(string memory _receiptId) public view returns(string memory, string memory){
+
+    function getReceiptInformation(string memory _receiptId)
+        public
+        view
+        returns (string memory, string memory, string memory)
+    {
         Receipt memory currentReceipt = receipts[_receiptId];
-        return (currentReceipt.receiptId, currentReceipt.customerId);
+        return (
+            currentReceipt.receiptId,
+            currentReceipt.customerId,
+            currentReceipt.storeId
+        );
     }
-    
-    function getItem(string memory _receiptId, uint _itemNumber) public view returns(string memory, string memory, uint, uint, bool, uint){
-        return (items[_receiptId][_itemNumber].receiptId, items[_receiptId][_itemNumber].productName, items[_receiptId][_itemNumber].price, items[_receiptId][_itemNumber].amount, items[_receiptId][_itemNumber].warranty, items[_receiptId][_itemNumber].warrantyTime);
+
+    function getItem(string memory _receiptId, uint256 _itemNumber)
+        public
+        view
+        returns (string memory, string memory, uint256, uint256, bool, uint256)
+    {
+        return (
+            items[_receiptId][_itemNumber].receiptId,
+            items[_receiptId][_itemNumber].productName,
+            items[_receiptId][_itemNumber].price,
+            items[_receiptId][_itemNumber].amount,
+            items[_receiptId][_itemNumber].warranty,
+            items[_receiptId][_itemNumber].warrantyTime
+        );
     }
-    
-    function getAllItemFromReceipt(string memory _receiptId) public view returns(Item[] memory){
+
+    function getAllItemFromReceipt(string memory _receiptId)
+        public
+        view
+        returns (Item[] memory)
+    {
         return items[_receiptId];
     }
-    
-    function countItem(string memory _receiptId) public view returns(uint) {
-      return items[_receiptId].length;
+
+    function countItem(string memory _receiptId) public view returns (uint256) {
+        return items[_receiptId].length;
     }
-    
-    function countReceipt() public view returns(uint) {
-      return ReceiptIds.length;
+
+    function countReceipt() public view returns (uint256) {
+        return ReceiptIds.length;
+    }
+
+    function listReceipt() public view returns (string[] memory) {
+        return ReceiptIds;
     }
 }
-// ["a123", "A021684"],[["a123", "Espresso", 4500, 1, false, 0 ],["a123", "IPhoneXI", 4500000, 1, true, 365 ]]
+// test input
+// ["a123", "A021684", "asdqsd3"],[["a123", "Espresso", 4500, 1, false, 0],["a123", "IPhoneXI", 4500000, 1, true, 365]]
+// ["AP245", "A021684", "ad12345"],[["AP245", "MacBook Pro", 4599000, 1, true, 365],["AP245", "IPhoneXI", 4500000, 1, true, 365]]
+
+// มันจะมีฟังก์ชันเรียกดูค่าที่ solidity มันทำให้เองเช่น items, resceipts ลองใช้ดู outpu มันจะต่างกัน เปรียบเทียบกับเมธอดที่เราสร้างแล้วเลือกใช้ให้เหมาะสม
+
+// ไปปรับ
+// Receipt ควรจะเก็บ ID ลูกค้า กับร้านค้ามั้ย
+// มันบันหมาทึกหมายเลขใบเสร็จซ้ำได้ เพราะฉะนั้นต้องดึง listReceipt มาเช็คก่อนว่าหมายเลขที่จะบันทึกมันมีซ้ำมั้ย กันพลาด ดักเคสไว้
+// หรือไม่ก็เช็คใน DB ก่อนว่าบันทึกได้มั้ย ถ้า id มันซ้ำมันบันทึกไม่ได้อยู๋แล้ว ก็ดักเคส ลำดับขั้นตอนเอาว่าควรจะดักเออเร่ออะไรยังไงก่อนหลัง
